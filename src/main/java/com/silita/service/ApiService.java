eb66d6f24498da0f23def92ccd55bf4e2669c795
zhushuai <cinnabar_s@sina.com>
2018-07-10 21:20:39 +0800

    修改

--- src/main/java/com/silita/service/ApiService.java
+++ src/main/java/com/silita/service/ApiService.java
@@ -31,13 +31,12 @@ public class ApiService {
             return resultMap;
         }
 
-        if(companyMap.get("regisAddress").toString().contains("湖南")){
-            companyMap.put("tabCode","hunan");
-        }else {
+        if (companyMap.get("regisAddress").toString().contains("湖南")) {
+            companyMap.put("tabCode", "hunan");
+        } else {
             //TODO: 获取人员表
             companyMap.put("tabCode", tbCompanyMapper.queryProvinceCode(MapUtils.getString(companyMap, "regisAddress")));
         }
-
 
 
         Page page = new Page();
@@ -72,55 +71,41 @@ public class ApiService {
 
         // TODO: 查询资质
         List<Map<String, Object>> list = new ArrayList<>();
-        Map<String, List<Map<String, Object>>> qualMap = queryCompanyQualification(companyMap);
-        Set<String> set = qualMap.keySet();
-        for (String key : set) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("qualType", key);
-            map.put("list", qualMap.get(key));
-            list.add(map);
-        }
-        resultMap.put("data",list);
+        list = queryCompanyQualification(companyMap);
+        resultMap.put("data", list);
         resultMap.put("code", 1);
         resultMap.put("msg", "操作成功!");
         return resultMap;
     }
 
-    public Map<String, List<Map<String, Object>>> queryCompanyQualification(Map<String, Object> param) {
-        Map<String, List<Map<String, Object>>> qualMap = new HashMap<>();
+    public List<Map<String, Object>> queryCompanyQualification(Map<String, Object> param) {
         // TODO: 18/4/10 需要关联标准资质表
         List<Map<String, Object>> list = tbCompanyMapper.queryCompanyQual(param);
-        for (Map<String, Object> qual:list){
+        List<Map<String, Object>> qualList = new ArrayList<>();
+        for (Map<String, Object> qual : list) {
             if (null != qual.get("qualType") && null != qual.get("range")) {
-                String key = MapUtils.getString(qual,"qualType");
-                List<Map<String, Object>> qualList;
-                if (qualMap.get(key) != null) {
-                    qualList = qualMap.get(key);
-                } else {
-                    qualList = new ArrayList<>();
-
+                String[] range = MapUtils.getString(qual, "range").split(",");
+                for (int i = 0; i < range.length; i++) {
+                    Map<String,Object> map = initMap(qual);
+                    map.put("qualName", range[i]);
+                    qualList.add(map);
                 }
-                String[] range = MapUtils.getString(qual,"range").split("；");
-                if (range.length < 2) {
-                    range = MapUtils.getString(qual,"range").split(";");
-                }
-                if (range.length < 2) {
-                    range = MapUtils.getString(qual,"range").split("\\|");
-                }
-
-                if (range.length >= 2) {
-                    for (int i = 0; i < range.length; i++) {
-                        Map<String, Object> qualT = qual;
-                        qualT.put("qualName",range[i]);
-                        qualList.add(qualT);
-                    }
-                } else {
-                    qual.put("qualName",range[0]);
-                    qualList.add(qual);
-                }
-                qualMap.put(key, qualList);
             }
         }
-        return qualMap;
+        return qualList;
+    }
+
+
+    private Map<String,Object> initMap (Map<String,Object> param){
+        Map<String,Object> initMap = new HashMap<>();
+        initMap.put("tab",param.get("tab"));
+        initMap.put("certNo",param.get("certNo"));
+        initMap.put("certOrg",param.get("certOrg"));
+        initMap.put("certDate",param.get("certDate"));
+        initMap.put("validDate",param.get("validDate"));
+        initMap.put("url",param.get("url"));
+        initMap.put("comName",param.get("comName"));
+        initMap.put("qualType",param.get("qualType"));
+        return initMap;
     }
 }
